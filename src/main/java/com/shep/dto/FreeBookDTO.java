883ea210970916a2b5c85b8cab33aef780e62233
artyomshpl <47445048+artyomshpl@users.noreply.github.com>
2024-08-28 21:53:27 +0300

    replace @Data with necessary lombok annotations in entity n DTO

--- src/main/java/com/shep/dto/FreeBookDTO.java
+++ src/main/java/com/shep/dto/FreeBookDTO.java
@@ -1,9 +1,14 @@
 package com.shep.dto;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 public class FreeBookDTO {
     private Long id;
     private Long bookId;
